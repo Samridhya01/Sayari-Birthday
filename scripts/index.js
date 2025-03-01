d57b6f1837a73ca81850db6e9c8f5c0e7c3c5086
Samridhya Santra <samridhyasantra311@gmail.com>
2025-03-01 14:48:56 +0530

    Sayari Birthday 🎂 🥳

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'feb 12, 2025',
-  name: 'Ananya ♡'
+  birthdate: 'March 23, 2025',
+  name: 'Sayari ♡'
 };
 
 function hideEverything() {
